4ac4aab6d13b8077f0fe48e4ea5713f090c4c825
Ahmed Abdulrahman <104634941+abdurrahmanu@users.noreply.github.com>
2024-02-12 14:03:24 +0100

    Update dictionaryStore.js

--- src/dictionaryStore.js
+++ src/dictionaryStore.js
@@ -1,9 +1,68 @@
 import {ref, computed} from 'vue'
 
 export const dictionaryStore = () => {
-  
+  const word = ref('')
+  const wordData = ref({})
+  const formattedData = ref({})
+  const loadingData = ref(false)
+
+  const newSearch = (event) => {
+    word.value = event
+    wordData.value = {}
+    formattedData.value = {}
+  }
+
+  watch(word, (newVal, oldVal) => {
+   let localStorageEntry = wordInLocalStorage(word)
+
+   if (localStorageEntry.exists.value) {
+      formattedData.value =    localStorageEntry.wordData.value[0];
+   }
+
+   else {
+     getData()
+   }
+ })
+
+  const getData = async () => {
+    loadingData.value = true
+    wordData.value = await    fetchWordDefinitions("https://api.dictionaryapi.dev/api/v2/entries/en/" + word.value)
+  loadingData.value = false
+
+   if (wordData.value.data.wordData) {
+     formattedData.value = formatWordData(wordData.value.data)
+
+    //if local storage exists or not
+    if (localStorage.getItem('dictionaryHistoryArray')) {
+       let previousHistory = ref(JSON.parse(localStorage.getItem('dictionaryHistoryArray')))
+
+      if (previousHistory.value.length > 9) {
+        previousHistory.value.shift()
+        previousHistory.value.push(formattedData.value)
+        localStorage.setItem('dictionaryHistoryArray', JSON.stringify(previousHistory.value))
+      }
+
+      else {
+        previousHistory.value.push(formattedData.value)
+        localStorage.setItem('dictionaryHistoryArray', JSON.stringify(previousHistory.value))
+      }
+    }
+
+    else {
+      let newArray = []
+      newArray[0] = formattedData.value
+      localStorage.setItem('dictionaryHistoryArray', JSON.stringify(newArray))
+    }
+  }
+}
+
 
   return {
-    
+    word,
+    wordData,
+    formattedData,
+    loadedData,
+    getData,
+    newSearch,
   }
 }
